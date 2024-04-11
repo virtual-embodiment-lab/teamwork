8bf3092239fd1fe8f2445acc88d14a96cd9e5e05
Miki Matsumuro <beauty.epoch@gmail.com>
2024-04-11 13:42:54 -0400

    0411

--- Assets/Scripts/RoleTrigger.cs
+++ Assets/Scripts/RoleTrigger.cs
@@ -4,6 +4,7 @@ public class RoleTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("role");
         if (other.CompareTag("Player"))
         {
             RoleSelect startRoom = FindObjectOfType<RoleSelect>(); // Find the StartRoom script in the scene.
